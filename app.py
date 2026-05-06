a4fc0c17a2ebe6ed2a77032f742bfaae584a06a0
RCEEeee <rasperchikwete@gmail.com>
2026-05-06 08:16:27 -0700

    Fix import statement for pymysql module

--- app.py
+++ app.py
@@ -554,5 +554,5 @@ def add_dept():
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
-import pynysql
+import pymysql
 pymysql.install_as_MYSQLdb()
