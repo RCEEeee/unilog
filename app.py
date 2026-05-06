2ee61c4488cfc4a99a40d131675e39923f48dfee
RCEEeee <rasperchikwete@gmail.com>
2026-05-06 08:03:31 -0700

    Install pymysql as MySQLdb in app.py

--- app.py
+++ app.py
@@ -554,4 +554,5 @@ def add_dept():
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
-
+import pynysql
+pymysql.install_as_MYSQLdb()
